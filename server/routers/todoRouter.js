eee962ed912f313413760589c47697205954f20c
jerejk03 <jere.j.kaasalainen@gmail.com>
2025-11-09 17:00:58 +0200

    tehtävä 6 valmis

--- server/routers/todoRouter.js
+++ server/routers/todoRouter.js
@@ -1,4 +1,5 @@
 import { pool } from '../helper/db.js'
+import { auth } from '../helper/auth.js' 
 import { Router } from 'express'
 
 const router = Router()
@@ -12,7 +13,7 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', auth,(req, res,next) => {
     const { task } = req.body
 
     if (!task) {
@@ -22,13 +23,13 @@ router.post('/create', (req, res) => {
     pool.query('insert into task (description) values ($1) returning *', [task.description],
         (err, result) => {
             if (err) {
-                return res.status(500).json({ error: err.message })
+                return next(err)
             }
             res.status(201).json({ id: result.rows[0].id, description: task.description })
         })
 })
 
-router.delete('/delete/:id', (req, res, next) => {
+router.delete('/delete/:id',auth, (req, res, next) => {
     const { id } = req.params
 
     pool.query('delete from task WHERE id = $1',
